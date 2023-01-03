b6d3d1998d677f0d63a6cf4426641ebd65b5c578
Shubham-verma3199 <115473332+Shubham-verma3199@users.noreply.github.com>
2023-01-03 20:36:09 +0530

    Add files via upload

---
+++ adjacent_sum.py
@@ -0,0 +1,11 @@
+def largest_sum_nonadjacent(nums):
+  if not nums:
+    return 0
+
+  if len(nums) == 1:
+    return nums[0]
+
+  return max(largest_sum_nonadjacent(nums[1:]), nums[0] + largest_sum_nonadjacent(nums[2:]))
+print(largest_sum_nonadjacent([2, 4, 6, 2, 5]))  
+print(largest_sum_nonadjacent([5, 1, 1, 5]))  
+
